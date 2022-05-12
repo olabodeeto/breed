ece18e8505c448d6906df5b0a388ed0e57fb085f
etrustfund <technology@ayeorganization.com>
2022-05-12 21:34:07 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <label className="menu-icon" htmlFor="check">
+        <span className="nav-icon"></span>
+      </label>
+      <input type="checkbox" id="check" />
+      <div className="logo">
+        <h1>breedUI</h1>
+      </div>
+
+      <ul className="menu">
+        <li>Buttons</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+    </header>
+  );
+}
---
+++ src/pages/indexModule/Index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+export default function Index() {
+  return (
+    <main>
+      <section className="intro">
+        <h1 className="breed-black">breedUI</h1>
+        <h1 className="breed-gray-100">Bundle</h1>
+        <p className="breed-w-5/12">
+          breedUI is a bundle of ui kits, extendable react components and some
+          CSS utilities classes that helps making web interface development
+          breezy!
+        </p>
+      </section>
+    </main>
+  );
+}
